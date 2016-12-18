e8246f5ffa4e0fc4936730960e4b9aa14d716a0a
rocketpie <mail.l.conrad@gmail.com>
2016-12-18 21:40:54 +0100

    Added Assembly description

--- src/PsHtml/PsHtml/Properties/AssemblyInfo.cs
+++ src/PsHtml/PsHtml/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PsHtml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Http host application for powershell scripts")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PsHtml")]
